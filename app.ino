f2ad183ea992e44e4a01ed45be5ff59e74c93d91
Eli Fatsi <eli.fatsi@viget.com>
2016-08-29 11:17:12 -0600

    Fading and delay adjustments happening in real time

--- app.ino
+++ app.ino
@@ -1,7 +1,8 @@
 #include "neopixel/neopixel.h" // use for Build IDE
+#include "math.h"
 
 #define PIXEL_PIN D2
-#define PIXEL_COUNT 23
+#define PIXEL_COUNT 28
 #define PIXEL_TYPE WS2812B
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(200, PIXEL_PIN, PIXEL_TYPE);
@@ -17,6 +18,7 @@ void setup() {
     strip.setPixelColor(i, 0, 0, 120);
   }
 
+  strip.setBrightness(255);
   strip.show();
 }
 
@@ -28,36 +30,69 @@ void loop() {
   }
 }
 
-void runBlueCircle() {
-  for (int i = 5; i >= 1; i--) {
-    strip.setPixelColor(0, 2 * i, 2 * i, map(i, 0, 5, 255, 100));
-    strip.setPixelColor(PIXEL_COUNT - 1, 5 + i, 5 + i, map(i, 0, 5, 220, 100));
-    strip.setPixelColor(PIXEL_COUNT - 2, 5 + i, 5 + i, map(i, 0, 5, 220, 100));
-    strip.show();
-    delay(100);
-  }
+int lowRg = 0;
+int lowB = 120;
 
+int highHeadRg = 30;
+int highHeadB  = 255;
+int highTailRg = 30;
+int highTailB  = 255;
+
+int fadeCount = 8;
+
+void runBlueCircle() {
   for (int i = 0; i < PIXEL_COUNT * 2; i++) {
     for (int j = 0; j < PIXEL_COUNT; j++) {
-      if (i == j) {
-        strip.setPixelColor(j, 0, 0, 255);
+      if ((i - j) % PIXEL_COUNT == 0) {
+        strip.setPixelColor(j, leadColor(i));
       } else if ((PIXEL_COUNT + i - j) % PIXEL_COUNT == 1) {
-        strip.setPixelColor(j, 5, 5, 220);
+        strip.setPixelColor(j, tailColor(i));
       } else if ((PIXEL_COUNT + i - j) % PIXEL_COUNT == 2) {
-          strip.setPixelColor(j, 5, 5, 220);
+          strip.setPixelColor(j, tailColor(i));
+      } else if ((PIXEL_COUNT + i - j) % PIXEL_COUNT == 3) {
+          strip.setPixelColor(j, tailColor(i));
       } else {
-        strip.setPixelColor(j, 10, 10, 100);
+        strip.setPixelColor(j, lowRg + 15, lowRg, lowB);
       }
     }
     strip.show();
-    delay(100);
+    delay(stepDelay(i));
   }
+}
 
-  for (int i = 1; i <= 5; i++) {
-    strip.setPixelColor(PIXEL_COUNT - 1, 2 * i, 2 * i, map(i, 0, 5, 255, 100));
-    strip.setPixelColor(PIXEL_COUNT - 2, 5 + i, 5 + i, map(i, 0, 5, 220, 100));
-    strip.setPixelColor(PIXEL_COUNT - 3, 5 + i, 5 + i, map(i, 0, 5, 220, 100));
-    strip.show();
-    delay(100);
+int stepDelay(int i) {
+  // return 35 + pow((0.16 * i) - 4, 2);
+  return 30 + pow((0.19 * i) - 5, 2);
+}
+
+uint32_t leadColor(int i) {
+  int rg, b;
+
+  if (i <= fadeCount) {
+    rg = max(0,   map(i + 1, 0, fadeCount, lowRg, highHeadRg));
+    b  = min(255, map(i + 1, 0, fadeCount, lowB, highHeadB));
+  } else if (i >= (PIXEL_COUNT * 2) - fadeCount) {
+    rg = max(0,   map(i + 1, (PIXEL_COUNT * 2) - fadeCount, (PIXEL_COUNT * 2), highHeadRg, lowRg));
+    b  = min(255, map(i + 1, (PIXEL_COUNT * 2) - fadeCount, (PIXEL_COUNT * 2), highHeadB, lowB));
+  } else {
+    rg = highHeadRg;
+    b  = highHeadB;
   }
+  return strip.Color(rg, rg, b);
+}
+
+uint32_t tailColor(int i) {
+  int rg, b;
+
+  if (i <= fadeCount) {
+    rg = max(0,   map(i + 1, 0, fadeCount, lowRg, highTailRg));
+    b  = min(255, map(i + 1, 0, fadeCount, lowB, highTailB));
+  } else if (i >= (PIXEL_COUNT * 2) - fadeCount) {
+    rg = max(0,   map(i + 1, (PIXEL_COUNT * 2) - fadeCount, (PIXEL_COUNT * 2), highTailRg, lowRg));
+    b  = min(255, map(i + 1, (PIXEL_COUNT * 2) - fadeCount, (PIXEL_COUNT * 2), highTailB, lowB));
+  } else {
+    rg = highTailRg;
+    b  = highTailB;
+  }
+  return strip.Color(rg, rg, b);
 }
